ee4f798139296aa4aebe1f445909cb3af0493df4
Luna Mendes <lkmnds@gmail.com>
2017-06-10 04:55:03 -0300

    Add error when authenticating with user/pass to discord

--- cord/http.py
+++ cord/http.py
@@ -14,6 +14,8 @@ class HTTP:
 
     async def gateway_url(self, *, version=7, encoding='json') -> str:
         """Returns the gateway URL used for connecting to the gateway."""
+
+        # automatic login if needed.
         if self.token is None:
             await self.login()
 
@@ -21,17 +23,24 @@ class HTTP:
             return (await resp.json())['url'] + f'?v={version}&encoding={encoding}'
 
     async def login(self):
+        """Gets a token using the auth endpoint."""
+
+        if 'discordapp' in self.api_root:
+            log.warning('Won\'t use user/pass login on discord.')
+            return
+
         _payload = {
             'email': self.email,
             'password': self.password,
         }
+
         async with self.session.post(self.route('/auth/login'), data=json.dumps(_payload)) as resp:
             j = await resp.json()
             self.token = j['token']
 
     async def get(self, path):
         """Makes a GET request.
- 
+
         Returns
         -------
         dict:
